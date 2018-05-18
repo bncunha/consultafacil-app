4d610a765f49c0d4ee2581c2911d2c3e898f4d51
Bruno Cunha <rocks.bn@gmail.com>
2018-05-17 23:49:13 -0300

    Criação da página de perfil

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -27,7 +27,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage, icon: 'home' },
-      { title: 'Pesquizar Médico', component: PesquizaPage, icon: 'search' },
+      { title: 'Pesquisar Médico', component: PesquizaPage, icon: 'search' },
       { title: 'Perfil', component: PerfilPage, icon: 'md-contact' }
       /*{ title: 'List', component: ListPage },
       { title: 'Login', component: LoginPage},
@@ -48,7 +48,8 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    // this.nav.setRoot(page.component);
+    this.nav.push(page.component);
     console.log(page);
   }
   
